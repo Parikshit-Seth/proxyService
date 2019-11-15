0fdc9c13cedf5075f656ad21f3b0aa79b7284cfa
PARIKSHIT SETH <parikshit.seth@aa.com>
2019-11-14 20:35:13 -0600

    moved the method

--- src/main/java/com/proxy/controller/ProxyController.java
+++ src/main/java/com/proxy/controller/ProxyController.java
@@ -14,28 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 public class ProxyController {
     @Autowired
     private RemoteService service;
-    @Autowired
-    private HttpServletRequest request;
+
 
     @RequestMapping(value = "/proxy/**", method = RequestMethod.GET)
     public ResponseEntity<String> greeting(@RequestHeader("User-Agent") String agent) {
-        String path = extractFilePath(request);
-        return service.invokeGet(agent, path);
+        return service.invokeGet(agent);
     }
 
     @RequestMapping(value = "/proxy/**", method = RequestMethod.POST)
     public ResponseEntity<String> greeting(@RequestHeader("User-Agent") String agent,
                                            @RequestBody String body) {
-        String path = extractFilePath(request);
-        return service.invokePost(agent, path, body);
+        return service.invokePost(agent, body);
     }
 
-    private String extractFilePath(HttpServletRequest request) {
-        String path = (String) request.getAttribute(
-                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        String bestMatchPattern = (String) request.getAttribute(
-                HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
-        AntPathMatcher apm = new AntPathMatcher();
-        return StringUtils.replace(apm.extractPathWithinPattern(bestMatchPattern, path),":/","://");
-    }
+
 }
--- src/main/java/com/proxy/service/RemoteService.java
+++ src/main/java/com/proxy/service/RemoteService.java
@@ -1,24 +1,44 @@
 package com.proxy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class RemoteService {
     public static final String USER_AGENT = "User-Agent";
-    RestTemplate restTemplate = new RestTemplate();
-    public ResponseEntity invokeGet(String header, String uri) {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public ResponseEntity invokeGet(String header) {
         HttpHeaders httpHeaders =  new HttpHeaders();
         httpHeaders.set(USER_AGENT, header);
         HttpEntity<String> request = new HttpEntity<>(httpHeaders);
-        return restTemplate.exchange(uri, HttpMethod.GET, request, String.class);
+        return restTemplate.exchange(getURI(), HttpMethod.GET, request, String.class);
     }
 
-    public ResponseEntity invokePost(String header, String uri, String body) {
+    public ResponseEntity invokePost(String header, String body) {
         HttpHeaders httpHeaders =  new HttpHeaders();
         httpHeaders.set(USER_AGENT, header);
         HttpEntity<String> request = new HttpEntity<>(body, httpHeaders);
-        return restTemplate.exchange(uri, HttpMethod.POST, request, String.class);
+        return restTemplate.exchange(getURI(), HttpMethod.POST, request, String.class);
+    }
+
+    private String getURI() {
+        String path = (String) request.getAttribute(
+                HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        String bestMatchPattern = (String) request.getAttribute(
+                HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        AntPathMatcher apm = new AntPathMatcher();
+        return StringUtils.replace(apm.extractPathWithinPattern(bestMatchPattern, path),":/","://");
     }
 }
